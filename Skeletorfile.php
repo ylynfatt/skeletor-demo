bae684db5891f7ce953fda75758552feb972a01a
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2025-01-25 18:41:15 -0500

    Fix an deprecation warning in Skeletorfile

--- Skeletorfile.php
+++ Skeletorfile.php
@@ -52,7 +52,7 @@ return function (Skeletor $skeletor) {
                 createInertiaApp({
                 resolve: name => {
                     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-                    return pages[`./Pages/${name}.vue`]
+                    return pages[`./Pages/\${name}.vue`]
                 },
                 setup({ el, App, props, plugin }) {
                     createApp({ render: () => h(App, props) })
